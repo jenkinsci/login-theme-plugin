359829f7549d1eb9fde6c8608804d4b4afb18ac4
Sridhar <srid01234567@gmail.com>
2024-02-16 11:38:14 +0530

    Upgrade to JUnit-5

--- src/test/java/io/jenkins/plugins/logintheme/LoginThemeTest.java
+++ src/test/java/io/jenkins/plugins/logintheme/LoginThemeTest.java
@@ -1,63 +1,58 @@
 package io.jenkins.plugins.logintheme;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.ClassRule;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.junit.Before;
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import org.kohsuke.stapler.StaplerRequest;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
 
+
+@WithJenkins
 public class LoginThemeTest {
 
-    @ClassRule
-    public static JenkinsRule jenkinsRule = new JenkinsRule();
-
-    public LoginThemeTest() {}
-
-    private LoginTheme plugin;
-
-    @Before
-    public void setUp() throws IOException {
-        plugin = new LoginTheme();
-    }
-
     @Test
-    public void getHeadAndSetHead() {
+    public void getHeadAndSetHead(JenkinsRule jenkinsRule) {
+        LoginTheme plugin = new LoginTheme();
         plugin.setHead("Head");
         assertEquals("Head", plugin.getHead());
     }
 
     @Test
-    public void getHeaderAndSetHeader() {
+    public void getHeaderAndSetHeader(JenkinsRule jenkinsRule) {
+        LoginTheme plugin = new LoginTheme();
         plugin.setHeader("Header");
         assertEquals("Header", plugin.getHeader());
     }
 
     @Test
-    public void getFooterAndSetFooter() {
+    public void getFooterAndSetFooter(JenkinsRule jenkinsRule) {
+        LoginTheme plugin = new LoginTheme();
         plugin.setFooter("Footer");
         assertEquals("Footer", plugin.getFooter());
     }
 
     @Test
-    public void getBrandingAndSetBranding() {
+    public void getBrandingAndSetBranding(JenkinsRule jenkinsRule) {
+        LoginTheme plugin = new LoginTheme();
         plugin.setBranding("Branding");
         assertEquals("Branding", plugin.getBranding());
     }
 
     @Test
-    public void getCustomCSSAndSetCustomCSS() {
+    public void getCustomCSSAndSetCustomCSS(JenkinsRule jenkinsRule) {
+        LoginTheme plugin = new LoginTheme();
         plugin.setCustomCSS("Custom CSS");
         assertEquals("Custom CSS", plugin.getCustomCSS());
     }
 
     @Test
-    public void testConfigure() throws Exception {
+    public void testConfigure(JenkinsRule jenkinsRule) throws Exception {
+        LoginTheme plugin = new LoginTheme();
 
         StaplerRequest staplerRequest = mock(StaplerRequest.class);
 
